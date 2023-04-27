ac8524c5cdedc4aa3f52ad14df511f63880740c2
q-hwang <qhwang@stanford.edu>
2023-04-26 18:16:58 -0700

    readme

--- LLM.py
+++ LLM.py
@@ -7,10 +7,12 @@ from helm.proxy.services.remote_service import RemoteService
 from functools import partial
 
 # CRFM API: https://github.com/stanford-crfm/helm/blob/main/demo.py
-auth = Authentication(api_key=open("crfm_api_key.txt").read().strip())
-service = RemoteService("https://crfm-models.stanford.edu")
-account: Account = service.get_account(auth)
-
+try:
+    auth = Authentication(api_key=open("crfm_api_key.txt").read().strip())
+    service = RemoteService("https://crfm-models.stanford.edu")
+    account: Account = service.get_account(auth)
+except:
+    print("Warning: CRFM API key not found. Please add your API key to crfm_api_key.txt if you want to use the CRFM API.")
 def complete_text_crfm(prompt, stop_sequences = None, model="openai/gpt-4-0314",  max_tokens_to_sample=2000, temperature = 0.2, **kwargs):
 
     request = Request(
@@ -25,7 +27,10 @@ def complete_text_crfm(prompt, stop_sequences = None, model="openai/gpt-4-0314",
     return completion
 
 # anthropic API:
-c = anthropic.Client(open("claude_api_key.txt").read().strip())
+try:
+    c = anthropic.Client(open("anthropic_api_key.txt").read().strip())
+except:
+    print("Warning: Anthropic API key not found. Please add your API key to anthropic_api_key.txt if you want to use the Anthropic API.")
 def complete_text_claude(prompt, stop_sequences=[anthropic.HUMAN_PROMPT], model="claude-v1", max_tokens_to_sample =  2000, temperature=0.2,**kwargs):
     resp = c.completion(
         prompt=f"{anthropic.HUMAN_PROMPT} {prompt} {anthropic.AI_PROMPT}",
@@ -39,7 +44,10 @@ def complete_text_claude(prompt, stop_sequences=[anthropic.HUMAN_PROMPT], model=
     return completion
 
 # openai API:
-openai.api_key = open("openai_api_key.txt").read().strip()
+try:
+    openai.api_key = open("openai_api_key.txt").read().strip()
+except:
+    print("Warning: OpenAI API key not found. Please add your API key to openai_api_key.txt if you want to use the OpenAI API.")
 def complete_text_openai(prompt, stop_sequences=None, model="text-davinci-003", max_tokens_to_sample = 2000, temperature=0.2, **kwargs):
     completion = openai.Completion.create(
         prompt=prompt,
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AI for Research
 
 This repo contains various GPT based tools for research, such as literature review and citation generation. Please feel free to contribute either tools or tool request!
+
+# Usage
+
+`LLM.py` manages API calls. Place your keys in `<openai/anthopic/crfm>_api_key.txt` to use the desired API, and point `complete_text_fast` and `complete_text_slow` to desired API call function.
+
+`example.py` include demos for current tools.
